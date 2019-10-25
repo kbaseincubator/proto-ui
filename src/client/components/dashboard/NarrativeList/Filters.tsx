782275f19ad60a507cd892b2b710e524deb9cd4e
Akiyo <flying.unicorn.sf@gmail.com>
2019-10-25 10:19:41 -0700

    working on filter.js mod packagejson

--- src/client/components/dashboard/NarrativeList/Filters.tsx
+++ src/client/components/dashboard/NarrativeList/Filters.tsx
@@ -5,6 +5,16 @@ import React, {Component} from 'react';
 import {FilterDropdown} from '../../generic/FilterDropdown';
 import {SearchInput} from '../../generic/SearchInput';
 
+interface State {
+  searchParams: {
+    term: string,
+    sort: string | null,
+  },
+}
+interface Props {
+  onSetSearch: (searchParams: State['searchParams'] ) => void;
+  loading: boolean;
+}
 /**
  * Filter bar for searching and sorting data results
  * state:
@@ -17,8 +27,8 @@ import {SearchInput} from '../../generic/SearchInput';
  * callbacks:
  * - onSetSearch - some filter has been applied to trigger a new search
  */
-export class Filters extends Component {
-  constructor(props) {
+export class Filters extends Component<Props, State> {
+  constructor(props:Props) {
     super(props);
     this.state = {
       searchParams: {
@@ -29,7 +39,7 @@ export class Filters extends Component {
   }
 
   // Handle an onSetVal event from SearchInput
-  handleSearch(val) {
+  handleSearch(val:string) {
     const searchParams = this.state.searchParams;
     searchParams.term = val;
     this.setState({searchParams});
@@ -39,7 +49,7 @@ export class Filters extends Component {
   }
 
   // Handle an onSelect event from FilterDropdown
-  handleFilter(idx, val) {
+  handleFilter(idx:number, val:string) {
     const searchParams = this.state.searchParams;
     searchParams.sort = val;
     this.setState({searchParams});
@@ -48,13 +58,13 @@ export class Filters extends Component {
     }
   }
 
-  render(props) {
+  render() {
     const dropdownItems = ['Newest', 'Oldest', 'Recently updated', 'Least recently updated'];
     return (
       <div className='bg-light-gray flex justify-between'>
         {/* Left-aligned actions (eg. search) */}
         <div className='pa3'>
-          <SearchInput loading={Boolean(props.loading)} onSetVal={this.handleSearch.bind(this)} />
+          <SearchInput loading={Boolean(this.props.loading)} onSetVal={this.handleSearch.bind(this)} />
         </div>
 
         {/* Right-aligned actions (eg. filter dropdown) */}
