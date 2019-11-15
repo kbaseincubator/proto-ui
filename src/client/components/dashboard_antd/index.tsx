d843ac951d1fdc8a4a31930bafc1cda45d3e4387
Jay R Bolton <jayrbolton@gmail.com>
2019-11-15 12:27:40 -0800

    WIP

--- src/client/components/dashboard_antd/index.tsx
+++ src/client/components/dashboard_antd/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { List, Avatar, Tabs, Input, Menu, Dropdown, Icon, Col, Row } from 'antd';
+import { Table, Avatar, Tabs, Input, Menu, Icon, Col, Row, Descriptions } from 'antd';
 import 'antd/dist/antd.css';
+import moment from 'moment';
 
 const { TabPane } = Tabs;
 const { Search } = Input;
@@ -13,15 +14,27 @@ import {
 import { getUsername } from '../../utils/auth';
 
 interface NarrativeItem {
-  title: string;
-  desc: string;
+  name: string;
+  author: string;
+  updated: string;
+  created: string;
+  key: string;
+  cell_count: number;
+  object_count: number;
+  is_public: boolean;
 }
 
 export interface SearchResult {
   _source: {
     narrative_title: string,
     creator: string,
-    creation_date: string
+    creation_date: string,
+    timestamp: number,
+    access_group: number,
+    obj_id: number,
+    cells: Array<Object>;
+    data_objects: Array<Object>;
+    is_public: boolean;
   }
 }
 
@@ -29,11 +42,41 @@ interface State {
   searchParams: SearchParams;
   loading: boolean;
   items: Array<NarrativeItem>;
+  selectedIdx: number;
 }
 
 interface Props {}
 
+
+// -- Global constants
 const PAGE_SIZE = 20;
+// Column configuration for the narrative table
+const TABLE_COLS = [
+  {
+    title: 'Narrative',
+    dataIndex: 'name'
+  },
+  {
+    title: 'Author',
+    dataIndex: 'author'
+  },
+  /*
+  {
+    title: 'Created',
+    dataIndex: 'created'
+  },
+  */
+  {
+    title: 'Updated',
+    dataIndex: 'updated'
+  }
+];
+// Localized date format
+// eg. 9/4/2019 8:30pm
+const DATE_FORMAT = "L LTS"
+// Background color for a selected row
+const SELECTED_BG_COLOR = "#e6f7ff";
+
 
 // Ant design version of the dashboard
 export class DashboardAntd extends React.Component<Props, State> {
@@ -49,6 +92,7 @@ export class DashboardAntd extends React.Component<Props, State> {
       },
       items: [],
       loading: false,
+      selectedIdx: 0,
     };
   }
 
@@ -69,9 +113,16 @@ export class DashboardAntd extends React.Component<Props, State> {
         if (resp && resp.hits) {
           const total = resp.hits.total;
           const items = resp.hits.hits.map((item: SearchResult) => {
+            console.log('item', item);
             return {
-              title: item._source.narrative_title,
-              desc: `Created on ${item._source.creation_date} by ${item._source.creator}`
+              name: item._source.narrative_title,
+              author: item._source.creator,
+              created: moment(item._source.creation_date).format(DATE_FORMAT),
+              updated: moment(item._source.timestamp).format(DATE_FORMAT),
+              key: `${item._source.access_group}/${item._source.obj_id}`,
+              cell_count: item._source.cells.length,
+              object_count: item._source.data_objects.length,
+              is_public: item._source.is_public
             }
           })
           this.setState({ items: items })
@@ -98,10 +149,17 @@ export class DashboardAntd extends React.Component<Props, State> {
 
   handleTabChange() {
     console.log('tab change');
+    this.setState({ items: [] });
+  }
+
+  handleClickRow(idx: number) {
+    this.setState({
+      selectedIdx: idx
+    })
   }
 
   render() {
-    // const {} = this.state;
+    const selected = this.state.items[this.state.selectedIdx];
     const menu = (
       <Menu>
         <Menu.Item>
@@ -127,7 +185,7 @@ export class DashboardAntd extends React.Component<Props, State> {
       </Menu>
     );
     return (
-      <Col span={12} style={{ padding: '2rem' }}>
+      <div style={{padding: '2rem', minWidth: '1024px' }}>
         <Tabs onChange={this.handleTabChange.bind(this)} type="card">
           <TabPane tab="My narratives" key="1">
           </TabPane>
@@ -140,36 +198,68 @@ export class DashboardAntd extends React.Component<Props, State> {
         </Tabs>
 
         <Row>
-          <Col span={8}>
+          <Col span={12} style={{paddingRight: '2rem' }}>
             <Search placeholder="Search narratives" onSearch={value => console.log(value)} enterButton />
-          </Col>
-
-          <Col span={12}/>
-
-          <Col span={4}>
-            <Dropdown overlay={menu}>
-              <a className="ant-dropdown-link" href="#">
-                Sort <Icon type="down" />
-              </a>
-            </Dropdown>
           </Col>
         </Row>
 
-        <List
-            itemLayout="horizontal"
-            dataSource={this.state.items}
-            renderItem={item => (
-                <List.Item>
-                    <List.Item.Meta
-                        title={<a href="http://spacejam.com">{item.title}</a>}
-                        description={item.desc}
-                    />
-                </List.Item>
-            )}
-        />
-      </Col>
+        <Row style={{ marginTop: '2rem' }}>
+          <Col span={12} style={{ paddingRight: '2rem' }}>
+            <Table
+                loading={this.state.loading}
+                columns={TABLE_COLS}
+                dataSource={this.state.items}
+                onRow={(record, rowIndex) => {
+                  return {
+                    onClick: ev => this.handleClickRow(rowIndex),
+                    style: {
+                      cursor: 'pointer',
+                      backgroundColor: selected && selected.key === record.key ? SELECTED_BG_COLOR : 'white'
+                    }
+                  };
+                }}
+            />
+          </Col>
+
+          { narrativeDetails(this, selected) }
+        </Row>
+      </div>
     );
   }
+}
+
+
+function narrativeDetails (cmp: DashboardAntd, item: NarrativeItem) {
+  if (!item) {
+    return (<div></div>);
+  }
+  return (
+    <Col span={12}>
+      <h3>{ item.name }</h3>
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Overview" key="1" />
+        <TabPane tab="Data" key="2" />
+        <TabPane tab="Preview" key="3" />
+      </Tabs>
+      <Descriptions bordered layout="horizontal" column={1}>
+        <Descriptions.Item label="Author">
+          { item.author }
+        </Descriptions.Item>
+        <Descriptions.Item label="Created on">
+          { item.created }
+        </Descriptions.Item>
+        <Descriptions.Item label="Total cells">
+          { item.cell_count }
+        </Descriptions.Item>
+        <Descriptions.Item label="Data objects">
+          { item.object_count }
+        </Descriptions.Item>
+        <Descriptions.Item label="Visibility">
+          { item.is_public ? 'public' : 'private' }
+        </Descriptions.Item>
+      </Descriptions>
+    </Col>
+  );
 }
 
 const mountNode = document.getElementById('dashboard-antd');
