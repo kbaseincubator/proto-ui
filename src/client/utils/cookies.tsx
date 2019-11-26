16bd54fdc8d8b4f559e6bed5e8900c3b999f218d
Jay R Bolton <jayrbolton@gmail.com>
2019-11-25 16:49:28 -0800

    Get some kind of sign-out functionality working

--- src/client/utils/cookies.tsx
+++ src/client/utils/cookies.tsx
@@ -8,3 +8,9 @@ export function getCookie(name: string) {
   }
   throw new Error('Unable to fetch non-existent cookie: ' + name);
 }
+
+export function removeCookie(name: string) {
+  const date = new Date();
+  date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+  document.cookie = name + "=; expires=" + date.toUTCString() + "; path=/";
+}
