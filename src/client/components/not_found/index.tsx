897ce412fa653870cf3d04116e4be5bd9cf9633d
Akiyo <flying.unicorn.sf@gmail.com>
2019-12-04 15:57:54 -0800

    comment on PR30 and some

--- src/client/components/not_found/index.tsx
+++ src/client/components/not_found/index.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class NotFoundPage extends Component {
   render() {
     return (
-      <section className="ph4 mt3">
+      <section className="mt3 tc">
         <h1>Page Not Found</h1>
         <img src='/static/images/flapjack.png' alt='Confused Flapjack 404' />
         <p>
